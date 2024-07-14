36eb60a5c337b526ad647830a05117554d8a9c7d
saikr <saikr@Luffy>
2024-07-14 13:11:54 +0530

    changes made to pom file

--- SDETGit/src/test/java/testRunner/CucumberTestRunner.java
+++ SDETGit/src/test/java/testRunner/CucumberTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-tags="@smoke", 
+tags="", 
 features = {"src/test/resources/Feature"}, 
 glue = {"com.stepDefinitions"},
 plugin ={"pretty","html:target/htmlreport.html"})
